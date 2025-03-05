902012b0803947c3e32fb612200c11366f9fbbdf
Nisha R K <55803996+nishark90@users.noreply.github.com>
2025-03-05 21:23:34 +0530

    Add project files.

---
+++ ConsoleApp2 . even number/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2.even_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        /*{Find the leap year 2050 to 2000
+            int i;
+            for (i = 2050; i >= 2000; i--) 
+            { if (i % 4 ==0)
+                {
+                    Console.WriteLine(i);
+                    
+                }
+            }
+            Console.ReadKey();
+
+        }*/
+        {
+            //Print the square of 1 to 5? //
+            /* int i;
+             for(i=1; i<=5; i++)
+             {
+             Console.WriteLine(i*i);
+             }*/
+
+            //3.Print the odd number from 30 to 60? //
+            /*int i;
+            for (i = 30; i <= 60; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    Console.WriteLine(i);
+                }
+            */
+
+            //4.Print the no divisible by both 3 and 5? //
+            /*int i;
+            for (i = 1; i <= 50; i++)
+            {
+                if
+                    (i % 3 == 0 && i % 5 == 0)
+
+                    Console.WriteLine(i);
+            }*/
+
+            //5.print the star pattern? //
+            int i;
+            for (i = 1; i <= 3; i++) 
+            {
+                Console.WriteLine("***");
+            }
+
+                Console.ReadLine();
+        }
+           
+        
+        
+    }
+}
